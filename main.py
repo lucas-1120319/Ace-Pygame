e601173398675630c089f7c70ffac569d0915ace
lucasvermeulen <lucas.a.vermeulen@gmail.com>
2025-09-17 11:59:47 +0200

    Create basic screen

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    screen.fill("red")
+    pygame.display.flip()
+
+pygame.quit()
