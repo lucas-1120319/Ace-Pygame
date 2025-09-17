c4a2f92c291ddb93f48af6275a2f42f1714ba6f2
lucasvermeulen <lucas.a.vermeulen@gmail.com>
2025-09-17 12:49:46 +0200

    Create player

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-
-# pygame setup
+from entities.player import Player
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
@@ -10,6 +9,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
     screen.fill("red")
+    player_class = Player(screen)
     pygame.display.flip()
 
 pygame.quit()
