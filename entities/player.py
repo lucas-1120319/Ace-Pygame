addea267fb4f90e668dab5889b402f956d3df1f8
Lucas-932 <lucas@familie-reedijk.nl>
2025-09-17 14:43:10 +0200

    PlayerMovement

--- entities/player.py
+++ entities/player.py
@@ -2,9 +2,23 @@ import pygame
 
 class Player():
     def __init__(self, screen):
-        player_img = pygame.image.load("Assets/Player/Player.png")
-        playerRect = player_img.get_rect()
-        screen.blit(player_img, playerRect)
+        self.screen = screen
+        self.speed = 12
+        self.player_img = pygame.image.load("Assets/Player/Player.png")
+        self.playerRect = self.player_img.get_rect(center=(640,550))
+        print("spawn")
+        screen.blit(self.player_img, self.playerRect)
+
 
     def update(self):
-        pass
+        self.screen.blit(self.player_img, self.playerRect)
+    def move(self, direction):
+        self.playerRect.move_ip((self.speed * direction.x), (self.speed * direction.y))
+
+
+
+
+
+
+
+
