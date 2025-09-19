92c51a3a79f339bbf2c0ea6e8b995cc1d313caa3
Tariq-1121745 <112945979+TariqBenabou@users.noreply.github.com>
2025-09-19 12:20:45 +0200

    Sound Effect added when moving left or right

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from pygame import Vector2
+from pygame import mixer
 
 from entities.player import Player
+
 pygame.init()
+mixer.init()
+
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
@@ -11,20 +15,44 @@ clock = pygame.time.Clock()
 FRAMES_PER_SECOND = 60
 game_clock = pygame.time.Clock()
 player_class = Player(screen)
+
+# load sfx for player movement
+player_move_left_sfx = pygame.mixer.Sound("Assets/Sound_Effects/player-whoosh.wav")
+player_move_right_sfx = pygame.mixer.Sound("Assets/Sound_Effects/player-whoosh.wav")
+
+# set volume
+player_move_left_sfx.set_volume(0.5)
+player_move_right_sfx.set_volume(0.5)
+
+
 def key_pressed():
     pressed_keys = pygame.key.get_pressed()
+
     if pressed_keys[pygame.K_a]:
         player_class.move(Vector2(-1, 0))
+
     if pressed_keys[pygame.K_d]:
         player_class.move(Vector2(1, 0))
+
     player_class.update()
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            quit()
+
+        #play sound once when key is pressed down
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player_move_left_sfx.play(maxtime=200)
+            if event.key == pygame.K_d:
+                player_move_right_sfx.play(maxtime=200)
+
     screen.fill("red")
     key_pressed()
     pygame.display.flip()
     game_clock.tick(FRAMES_PER_SECOND)
+
 pygame.quit()
